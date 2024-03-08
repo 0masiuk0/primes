6e4b712fc38fd994709a78cbe29ba316398b1de7
0masiuk0 <90094421+0masiuk0@users.noreply.github.com>
2024-03-08 11:19:42 +0300

    folder structure change

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='primes',
-    version='0.1',
+    version='0.2',
     packages=['primes'],
     url='https://github.com/0masiuk0/primes',
     license='',
-    author='masuk',
+    author='masiuk',
     author_email='',
     description='Primes and factorization related functions with memorization',
     install_requires=['bitarray'])
